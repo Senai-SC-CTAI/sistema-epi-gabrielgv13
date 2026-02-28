f2c6d0a8aaf372104fc09ad591ddeb398fced22a
geni <142359275+gabrielgv13@users.noreply.github.com>
2026-02-28 00:02:23 -0300

    Adicionado geração de CSV de movimentações para utilização no Power Bi

--- relatorios/urls.py
+++ relatorios/urls.py
@@ -6,4 +6,5 @@ app_name = 'relatorios'
 urlpatterns = [
     path('', views.app_reports, name='app_reports'),
     path('configs/', views.app_configs, name='app_configs'),
+    path('download/movimentacoes-csv/', views.download_movimentacoes_csv, name='download_movimentacoes_csv'),
 ]
